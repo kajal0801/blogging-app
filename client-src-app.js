05c4204c5160553ec77dfdbea7a2c18d1004f43b
kajal0801 <144913795+kajal0801@users.noreply.github.com>
2025-07-07 18:11:55 +0530

    Create client-src-app.js

---
+++ client-src-app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>📝 Blogging App</h1>
+      <p>Welcome! This is the frontend of my blogging application. 🚧 Work in progress...</p>
+    </div>
+  );
+}
+
+export default App;
